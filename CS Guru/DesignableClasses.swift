25c2b351811a88f3569dec4bb214a16c881665bc
Abdul <tarabi@uw.edu>
2019-09-24 00:43:45 -0700

    Finished the homepage

--- CS Guru/DesignableClasses.swift
+++ CS Guru/DesignableClasses.swift
@@ -11,23 +11,60 @@ import UIKit
 @IBDesignable
 class DesignableButton: UIButton {
     
-    @IBInspectable var cornerRadius: CGFloat = 0{
+    @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet{
             self.layer.cornerRadius = cornerRadius
         }
     }
     
-    @IBInspectable var borderWidth: CGFloat = 0{
+    @IBInspectable var borderWidth: CGFloat = 0 {
         didSet{
             self.layer.borderWidth = borderWidth
         }
     }
     
-    @IBInspectable var borderColor: UIColor = UIColor.clear{
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
         didSet{
             self.layer.borderColor = borderColor.cgColor
         }
     }
+    
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet{
+            self.layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 0, height: 0) {
+        didSet{
+            self.layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet{
+            self.layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet{
+            self.layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var masksToBounds: Bool = false {
+        didSet{
+            self.layer.masksToBounds = masksToBounds
+        }
+    }
+    
+    @IBInspectable var shadowPath: UIBezierPath = UIBezierPath.init() {
+        didSet{
+            self.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.inputView!.layer.cornerRadius).cgPath
+        }
+    }
+    
 }
 
 @IBDesignable
